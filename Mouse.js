8041a833f8608ca01faa174d46d0ac129e4bbaf6
Dung Nguyen <heyimnu97@gmail.com>
2019-08-01 15:31:16 +0700

    Fix die method

--- Mouse.js
+++ Mouse.js
@@ -2,8 +2,12 @@ function Mouse(color, weight)
 {
 	this.color = color;
 	this.weight = weight;
+	this.isDead = false;
 }
 Mouse.prototype.sleep = function() {
 	console.log('Zzzz ...');
 };
+Mouse.prototype.die = function() {
+	this.isDead = true;
+}
 module.exports = Mouse;
